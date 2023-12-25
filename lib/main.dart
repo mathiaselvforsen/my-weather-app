27c3b7567f4ce923c036010febcd47a696d8b1c6
mathiaselvforsen <me224qh@student.lnu.se>
2023-12-25 13:51:12 +0100

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart';
+
 //Imports end
 
 // Function to run the app
